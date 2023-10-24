ac7e36cff710a1311d68ecccd63f6c16b9edfa3c
yikes <111034762+twilwa@users.noreply.github.com>
2023-10-24 11:46:33 -0700

    aider: Added functionality to add and delete comments in `agent-protocol-handler/src/index.jsx`.
    # Aider chat conversation:
    
    USER: hey can you modify our `agent-protocol-handler/src/index.jsx`
    
    ASSISTANT: Sure, I'd be happy to help. Could you please specify what modifications you would like to make to the `agent-protocol-handler/src/index.jsx` file?
    
    USER: let's modify `agent-protocol-handler/src/index.jsx` to include the functionality to delete or add comments in addition to fetching them for a given issue.
    
    ASSISTANT: Alright, to add the functionality to add and delete comments, we will need to modify the `agent-protocol-handler/src/index.jsx` file.
    
    Here are the steps we will follow:
    
    1. We will add a new function `addCommentForIssue` that will make a POST request to the Jira API to add a comment to a specific issue.
    2. We will modify the `App` component to include buttons for adding and deleting comments, and text fields to input the comment text and ID.
    
    Here are the changes:
    
    ```jsx
    agent-protocol-handler/src/index.jsx
    <<<<<<< HEAD
    import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
    import api, { route } from "@forge/api";
    import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext } from "@forge/ui";
    =======
    import ForgeUI, { render, Fragment, Text, IssuePanel, Button, TextField, useProductContext } from "@forge/ui";
    import api, { route } from "@forge/api";
    >>>>>>> updated
    ```
    
    ```jsx
    agent-protocol-handler/src/index.jsx
    <<<<<<< HEAD
    const deleteCommentForIssue = async (issueIdOrKey, commentId) => {
      const res = await api
        .asUser()
        .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment/${commentId}`, {
          method: 'DELETE',
        });
    
      if (!res.ok) {
        throw new Error(`Failed to delete comment with id ${commentId} for issue ${issueIdOrKey}`);
      }
    };
    =======
    const addCommentForIssue = async (issueIdOrKey, commentBody) => {
      const res = await api
        .asUser()
        .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`, {
          method: 'POST',
          body: JSON.stringify({
            body: commentBody,
          }),
        });
    
      if (!res.ok) {
        throw new Error(`Failed to add comment to issue ${issueIdOrKey}`);
      }
    };
    
    const deleteCommentForIssue = async (issueIdOrKey, commentId) => {
      const res = await api
        .asUser()
        .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment/${commentId}`, {
          method: 'DELETE',
        });
    
      if (!res.ok) {
        throw new Error(`Failed to delete comment with id ${commentId} for issue ${issueIdOrKey}`);
      }
    };
    >>>>>>> updated
    ```
    
    ```jsx
    agent-protocol-handler/src/index.jsx
    <<<<<<< HEAD
    const App = () => {
      return (
        <Fragment>
          <Text>Hello world!</Text>
        </Fragment>
      );
    };
    =======
    const App = () => {
      const { platformContext: { issueKey } } = useProductContext();
      const [commentBody, setCommentBody] = useState('');
      const [commentId, setCommentId] = useState('');
    
      const handleAddComment = async () => {
        await addCommentForIssue(issueKey, commentBody);
        setCommentBody('');
      };
    
      const handleDeleteComment = async () => {
        await deleteCommentForIssue(issueKey, commentId);
        setCommentId('');
      };
    
      return (
        <Fragment>
          <Text>Add or delete comments</Text>
          <TextField label="Comment body" value={commentBody} onChange={setCommentBody} />
          <Button text="Add comment" onClick={handleAddComment} />
          <TextField label="Comment ID to delete" value={commentId} onChange={setCommentId} />
          <Button text="Delete comment" onClick={handleDeleteComment} />
        </Fragment>
      );
    };
    >>>>>>> updated
    ```
    
    Please note that this is a basic implementation and does not include error handling or user feedback. You might want to enhance this according to your needs.

--- agent-protocol-handler/src/index.jsx
+++ agent-protocol-handler/src/index.jsx
@@ -1,6 +1,5 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, Button, TextField, useProductContext } from "@forge/ui";
 import api, { route } from "@forge/api";
-import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext } from "@forge/ui";
 
 
 const fetchCommentsForIssue = async (issueIdOrKey) => {
@@ -10,6 +9,21 @@ const fetchCommentsForIssue = async (issueIdOrKey) => {
 
   const data = await res.json();
   return data.comments;
+};
+
+const addCommentForIssue = async (issueIdOrKey, commentBody) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`, {
+      method: 'POST',
+      body: JSON.stringify({
+        body: commentBody,
+      }),
+    });
+
+  if (!res.ok) {
+    throw new Error(`Failed to add comment to issue ${issueIdOrKey}`);
+  }
 };
 
 const deleteCommentForIssue = async (issueIdOrKey, commentId) => {
@@ -25,9 +39,27 @@ const deleteCommentForIssue = async (issueIdOrKey, commentId) => {
 };
 
 const App = () => {
+  const { platformContext: { issueKey } } = useProductContext();
+  const [commentBody, setCommentBody] = useState('');
+  const [commentId, setCommentId] = useState('');
+
+  const handleAddComment = async () => {
+    await addCommentForIssue(issueKey, commentBody);
+    setCommentBody('');
+  };
+
+  const handleDeleteComment = async () => {
+    await deleteCommentForIssue(issueKey, commentId);
+    setCommentId('');
+  };
+
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Add or delete comments</Text>
+      <TextField label="Comment body" value={commentBody} onChange={setCommentBody} />
+      <Button text="Add comment" onClick={handleAddComment} />
+      <TextField label="Comment ID to delete" value={commentId} onChange={setCommentId} />
+      <Button text="Delete comment" onClick={handleDeleteComment} />
     </Fragment>
   );
 };
